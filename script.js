63e07a0732e4f85040922afececd2a694106c1b8
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-02-15 16:34:07 -0700

    html, basic css, and js

---
+++ script.js
@@ -0,0 +1,32 @@
+// VARIABLE DECLARATIONS
+var questionArray = [{
+        question: "1. Commonly used data types DO NOT include",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        question: "2. The condition in an if/else statement is enclosed within ___.",
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        question: "3. Arrays in JavaScript can be used to store ___",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "4. String values must be enclosed within ___ when being assigned to variables. ",
+        options: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        question: "5. A very useful tool used during development and debugging for printing content to the debugger is ___.",
+        options: ["JavaScript", "terminal/bash", "alerts", "console.log" ],
+        answer: "console.log"
+    }
+]
+
+var score = 0;
+var currentQuestion = 0;
+var timeLeft = ;
+var timer;
